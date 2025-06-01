f51c7ebdd0c19176d5419eb4a2f8272679395fd7
benisamuel <benisamuel566@gmail.com>
2025-06-01 22:02:29 +0200

    Added create contributiondto

--- src/main/java/com/workers/www/dto/CreateContributionDto.java
+++ src/main/java/com/workers/www/dto/CreateContributionDto.java
@@ -1,4 +1,10 @@
 package com.workers.www.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CreateContributionDto {
+    private Long assignment_id;
+    private Long user_id;
+    private String description;
 }
