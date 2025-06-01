ddf6e77e27b726cd7b3aaf99b3daa2117184772e
benisamuel <benisamuel566@gmail.com>
2025-06-01 19:56:54 +0200

    added assignment creation dto

--- src/main/java/com/workers/www/dto/CreateAssignmentDto.java
+++ src/main/java/com/workers/www/dto/CreateAssignmentDto.java
@@ -1,4 +1,16 @@
 package com.workers.www.dto;
 
+import com.workers.www.enums.AssignmentStatus;
+import com.workers.www.enums.Role;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class CreateAssignmentDto {
+    private Long user_id;
+    private String description;
+    private final LocalDateTime duration = LocalDateTime.now();
+    private AssignmentStatus status;
+    private Role role;
 }
