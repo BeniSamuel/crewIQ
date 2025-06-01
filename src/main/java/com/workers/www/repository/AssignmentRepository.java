754caaab06f2257676fa5d0936ff66a75c6a8765
benisamuel <benisamuel566@gmail.com>
2025-06-01 19:57:20 +0200

    improved methods in assignment repository interface

--- src/main/java/com/workers/www/repository/AssignmentRepository.java
+++ src/main/java/com/workers/www/repository/AssignmentRepository.java
@@ -3,6 +3,7 @@ package com.workers.www.repository;
 import com.workers.www.enums.AssignmentStatus;
 import com.workers.www.model.Assignment;
 import com.workers.www.model.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,6 +11,6 @@ import java.util.List;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     Assignment getAssignmentById (Long id);
-    List<Assignment> getAssignmentsByManager(User manager, Pageable pageable);
-    List<Assignment> getAssignmentsByStatus(AssignmentStatus status, Pageable pageable);
+    Page<Assignment> getAssignmentsByManager(User manager, Pageable pageable);
+    Page<Assignment> getAssignmentsByStatus(AssignmentStatus status, Pageable pageable);
 }
