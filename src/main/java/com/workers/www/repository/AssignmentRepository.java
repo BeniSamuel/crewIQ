480bdbcd13b92f95294fac05b325df80cc52702d
benisamuel <benisamuel566@gmail.com>
2025-06-02 21:18:00 +0200

    added searching method in assignment repostion #Shit2kAges

--- src/main/java/com/workers/www/repository/AssignmentRepository.java
+++ src/main/java/com/workers/www/repository/AssignmentRepository.java
@@ -3,6 +3,7 @@ package com.workers.www.repository;
 import com.workers.www.enums.AssignmentStatus;
 import com.workers.www.model.Assignment;
 import com.workers.www.model.User;
+import com.workers.www.enums.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     Assignment getAssignmentById (Long id);
     Page<Assignment> getAssignmentsByManager(User manager, Pageable pageable);
     Page<Assignment> getAssignmentsByStatus(AssignmentStatus status, Pageable pageable);
+    List<Assignment> findByStatusAndRole(AssignmentStatus status, Role role);
 }
