7e6dfe2147430d5dc687976266a7e587c5ecd7c0
benisamuel <benisamuel566@gmail.com>
2025-06-02 07:12:08 +0200

    added jwtutil for handling jsonwebtokens

--- src/main/java/com/workers/www/service/AuthService.java
+++ src/main/java/com/workers/www/service/AuthService.java
@@ -1,4 +1,39 @@
 package com.workers.www.service;
 
+import com.workers.www.dto.LoginUserDto;
+import com.workers.www.dto.RegisterUserDto;
+import com.workers.www.model.User;
+import com.workers.www.utils.ApiResponse;
+import com.workers.www.utils.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class AuthService {
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public User registerUser (RegisterUserDto registerUserDto) {
+        return this.userService.createUser(registerUserDto);
+    }
+
+    public ResponseEntity<ApiResponse<String>> loginUser (LoginUserDto loginUserDto) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginUserDto.getEmail(), loginUserDto.getPassword())
+            );
+
+            User user = this.userService.getUserByEmail(loginUserDto.getEmail());
+            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully logged in user", this.jwtUtil.generateToken(user.getEmail())));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, "Failed to log in user", null));
+        }
+    }
 }
--- src/main/java/com/workers/www/utils/JwtUtil.java
+++ src/main/java/com/workers/www/utils/JwtUtil.java
@@ -1,7 +1,23 @@
 package com.workers.www.utils;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtUtil {
+
+    @Value("${jwt.secret.key}")
+    private String SECRET_KEY;
+
+    public String generateToken (String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 50))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
 }
