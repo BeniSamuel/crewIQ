b303acaf480aef9e5fff6c04a77e4261cee46199
benisamuel <benisamuel566@gmail.com>
2025-06-02 21:18:56 +0200

    added security handlers such as userDetails, userDetailsService and JwtAuthFilter #CRG

--- src/main/java/com/workers/www/security/CustomUserDetailsService.java
+++ src/main/java/com/workers/www/security/CustomUserDetailsService.java
@@ -1,4 +1,26 @@
 package com.workers.www.security;
 
-public class CustomUserDetailsService {
+import com.workers.www.exception.NotFoundException;
+import com.workers.www.service.UserService;
+import lombok.AllArgsConstructor;
+import com.workers.www.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userService.getUserByEmail(username);
+        if (user != null) {
+            return new CustomUserDetails(user);
+        } else {
+            throw new NotFoundException("User not found!!!");
+        }
+    }
 }
