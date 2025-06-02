98551cf3c5100aadb46a790208829ddada8b028f
benisamuel <benisamuel566@gmail.com>
2025-06-02 21:16:02 +0200

    ain't adding the shit but just adding role authorization in filter chain

--- src/main/java/com/workers/www/config/SecurityConfig.java
+++ src/main/java/com/workers/www/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.workers.www.config;
 
+import com.workers.www.security.JwtAuthFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,10 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
@@ -20,8 +25,23 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/workers/auth/signup").permitAll()
+                        .requestMatchers("/api/workers/auth/login").permitAll()
+                        .requestMatchers("/api/workers/user/update/").hasRole("MANAGER")
+                        .requestMatchers("/api/workers/user/update/").hasRole("ADMIN")
+                        .requestMatchers("/api/workers/assignment/delete/").hasRole("MANAGER")
+                        .requestMatchers("/api/workers/assignment/delete/").hasRole("ADMIN")
+                        .requestMatchers("/api/workers/assignment/update/").hasRole("MANAGER")
+                        .requestMatchers("/api/workers/assignment/update/").hasRole("ADMIN")
+                        .requestMatchers("/api/workers/assignment/create").hasRole("MANAGER")
+                        .requestMatchers("/api/workers/assignment/create").hasRole("ADMIN")
+                        .requestMatchers("/api/workers/salary/create").hasRole("ADMIN")
+                        .requestMatchers("/api/workers/salary/create").hasRole("MANAGER")
+                        .requestMatchers("/api/workers/salary/update/").hasRole("MANAGER")
+                        .requestMatchers("/api/workers/salary/update/").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .build();
     }
@@ -31,3 +51,5 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
+
+
