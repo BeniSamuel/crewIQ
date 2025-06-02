9e1a3f7a37b1190c7bab7115bc8e18f11fae2dc4
benisamuel <benisamuel566@gmail.com>
2025-06-02 07:11:09 +0200

    added auth controller

--- src/main/java/com/workers/www/controller/AuthController.java
+++ src/main/java/com/workers/www/controller/AuthController.java
@@ -1,4 +1,31 @@
 package com.workers.www.controller;
 
+import com.workers.www.dto.LoginUserDto;
+import com.workers.www.dto.RegisterUserDto;
+import com.workers.www.model.User;
+import com.workers.www.service.AuthService;
+import com.workers.www.utils.ApiResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/workers/auth")
+@AllArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<User>> registerUser (@RequestBody RegisterUserDto registerUserDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true, "Successfully registered user", this.authService.registerUser(registerUserDto)));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<String>> loginUser (@RequestBody LoginUserDto loginUserDto) {
+        return this.authService.loginUser(loginUserDto);
+    }
 }
--- src/main/java/com/workers/www/controller/ContributionController.java
+++ src/main/java/com/workers/www/controller/ContributionController.java
@@ -1,14 +1,14 @@
 package com.workers.www.controller;
 
+import com.workers.www.dto.CreateContributionDto;
 import com.workers.www.model.Contribution;
 import com.workers.www.service.ContributionService;
 import com.workers.www.utils.ApiResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +18,14 @@ import java.util.List;
 public class ContributionController {
     private final ContributionService contributionService;
 
+    @GetMapping()
+    public ResponseEntity<ApiResponse<Page<Contribution>>> getContributionByCurrentUser (
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "Successfully obtained contribution", this.contributionService.getContributionForCurrentUser(page, size)));
+    }
+
     @GetMapping("/all")
     public ResponseEntity<ApiResponse<List<Contribution>>> getAllContribution () {
         return ResponseEntity.ok(new ApiResponse<>(true, "Successfully obtained all contributions", this.contributionService.getAllContribution()));
@@ -26,6 +34,48 @@ public class ContributionController {
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<Contribution>> getContributionById (@PathVariable Long id) {
         Contribution contribution = this.contributionService.getContributionById(id);
-
+        if (contribution != null) {
+            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully obtained the contribution", contribution));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to obtain contribution", null));
     }
+
+    @GetMapping("/user/{user_id}")
+    public ResponseEntity<ApiResponse<Page<Contribution>>> getContributionsByUser (
+            @PathVariable Long user_id,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Page<Contribution> contributions = this.contributionService.getContributionByUser(user_id, page, size);
+        if (!contributions.isEmpty()) {
+            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully obtained all contributions by user", contributions));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to obtain contribution by user", null));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<Contribution>> createContribution (@RequestBody CreateContributionDto createContributionDto) {
+        Contribution newContribution = this.contributionService.createContribution(createContributionDto);
+        if (newContribution != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true, "Successfully created contribution", newContribution));
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, "Failed to create contribution", null));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse<Boolean>> deleteContributionById (@PathVariable Long id) {
+        return this.contributionService.deleteContributionById(id) ?
+               ResponseEntity.ok(new ApiResponse<>(true, "Successfully deleted contribution", true)) :
+               ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to delete contribution", false));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponse<Contribution>> updateContributionById (@PathVariable Long id, @RequestBody CreateContributionDto createContributionDto) {
+        Contribution contribution = this.contributionService.updateContributionById(id, createContributionDto);
+        if (contribution != null) {
+            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully updated contribution", contribution));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to update contribution", null));
+    }
+
 }
