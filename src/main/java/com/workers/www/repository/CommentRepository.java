d8bc94ef5ec676c3a161de616ed22cfda34605aa
benisamuel <benisamuel566@gmail.com>
2025-06-02 08:16:28 +0200

    added page return type on comment repository

--- src/main/java/com/workers/www/repository/CommentRepository.java
+++ src/main/java/com/workers/www/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import com.workers.www.model.Assignment;
 import com.workers.www.model.Comment;
 import com.workers.www.model.Contribution;
 import com.workers.www.model.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,7 +12,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Comment getCommentById (Long id);
-    List<Comment> getCommentsByAuthor (User user, Pageable pageable);
-    List<Comment> getCommentsByContribution (Contribution contribution, Pageable pageable);
-    List<Comment> getCommentsByAssignment (Assignment assignment, Pageable pageable);
+    Page<Comment> getCommentsByAuthor (User user, Pageable pageable);
+    Page<Comment> getCommentsByContribution (Contribution contribution, Pageable pageable);
+    Page<Comment> getCommentsByAssignment (Assignment assignment, Pageable pageable);
 }
