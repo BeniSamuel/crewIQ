2641733799af34f1abc3858425ea8136a174bb70
benisamuel <benisamuel566@gmail.com>
2025-06-03 20:25:50 +0200

    added mailing service

--- src/main/java/com/workers/www/service/MailService.java
+++ src/main/java/com/workers/www/service/MailService.java
@@ -1,4 +1,22 @@
 package com.workers.www.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail (String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("benisamuel566@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
 }
