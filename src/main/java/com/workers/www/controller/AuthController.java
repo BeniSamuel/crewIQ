eabf177ca2e7bc4f33156531a148d148a571ca9b
benisamuel <benisamuel566@gmail.com>
2025-06-04 13:19:49 +0200

    added reset-password controller

--- src/main/java/com/workers/www/controller/AuthController.java
+++ src/main/java/com/workers/www/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.workers.www.controller;
 
+import com.workers.www.dto.ForgotPasswordDto;
 import com.workers.www.dto.LoginUserDto;
 import com.workers.www.dto.RegisterUserDto;
 import com.workers.www.model.User;
@@ -18,6 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<ApiResponse<Boolean>> resetPassword (@RequestBody ForgotPasswordDto forgotPasswordDto) {
+        return this.authService.resetPassword(forgotPasswordDto) ?
+               ResponseEntity.ok(new ApiResponse<>(true, "Successfully reset password", true)) :
+               ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to reset password invalid email", false));
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<User>> registerUser (@RequestBody RegisterUserDto registerUserDto) {
